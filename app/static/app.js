f9c8f43c752fe641312167f953caff14f8b412ee
Malcome Baggett <malcomebaggett@gmail.com>
2020-06-22 13:22:26 -0700

    Added Tables

--- app/static/app.js
+++ app/static/app.js
@@ -7,20 +7,29 @@ $(document).ready(function () {
 const form = document.querySelector('#baby-form')
 const milkType = document.querySelector('#milktype')
 const amount = document.querySelector('#amount')
-const feedList = document.querySelector('.collection')
+const feedBody = document.querySelector('#feed-body')
+const tableData = document.querySelector('#table-data')
+const feedTable = document.querySelector('#feed-table')
 
 form.addEventListener('submit', (e) => {
-    if (amount.value === "" || milkType.value === "") {
-        alert('Please check you inputs.')
+    // if (amount.value === "" || milkType.value === "") {
+    //     alert('Please check you inputs.')
+    // }
+    // get date and time
+    console.log(feedTable.rows[0].cells.length)
+    let date = moment().format("DD/MM/YYYY")
+    let time = moment().format("HH:mm")
+    const rowInput = [`${amount.value}ML`, milkType.value, date, time]
+
+    let row = feedTable.insertRow()
+    for (let i = 0; i <= rowInput.length - 1; i++) {
+        let cell = row.insertCell(i)
+        let text = document.createTextNode(rowInput[i])
+        cell.appendChild(text)
     }
-    // create li to append to ul
-    const li = document.createElement('li')
-    // add materialize .collection item class
-    li.className = "collection-item"
-    // creat textNode and append
-    li.appendChild(document.createTextNode(`Amount: ${amount.value} || Milk Type: ${milkType.value}`))
-    // append to ul
-    feedList.appendChild(li)
+    amount.value = ""
+    milkType.value = "Choose your option"
     e.preventDefault()
+
 
 })
