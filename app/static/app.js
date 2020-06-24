af6f01e2ec62e9989cbe113e935fe64d2ed45f2b
Malcome Baggett <malcomebaggett@gmail.com>
2020-06-24 13:26:34 -0700

    added frontend validation

--- app/static/app.js
+++ app/static/app.js
@@ -12,24 +12,40 @@ const tableData = document.querySelector('#table-data')
 const feedTable = document.querySelector('#feed-table')
 
 form.addEventListener('submit', (e) => {
-    // if (amount.value === "" || milkType.value === "") {
-    //     alert('Please check you inputs.')
-    // }
-    // get date and time
-    console.log(feedTable.rows[0].cells.length)
-    let date = moment().format("DD/MM/YYYY")
-    let time = moment().format("HH:mm")
-    const rowInput = [`${amount.value}ML`, milkType.value, date, time]
+    if (amount.value === "" || milkType.value === "") {
+        showError('Please check input!')
+    } else {
+        console.log(feedTable.rows[0].cells.length)
+        const date = moment().format("DD/MM/YYYY")
+        const time = moment().format("HH:mm")
+        const rowInput = [`${amount.value}ML`, milkType.value, date, time]
 
-    let row = feedBody.insertRow()
-    for (let i = 0; i <= rowInput.length - 1; i++) {
-        let cell = row.insertCell(i)
-        let text = document.createTextNode(rowInput[i])
-        cell.appendChild(text)
+        let row = feedBody.insertRow()
+        for (let i = 0; i <= rowInput.length - 1; i++) {
+            let cell = row.insertCell(i)
+            let text = document.createTextNode(rowInput[i])
+            cell.appendChild(text)
+        }
+        amount.value = ""
+        milkType.value = "Choose your option"
+
     }
-    amount.value = ""
-    milkType.value = "Choose your option"
     e.preventDefault()
-
-
 })
+
+function showError(error) {
+    const errDiv = document.createElement('div')
+    errDiv.className = 'center'
+    errDiv.id = 'err'
+    const cardContent = document.querySelector('.card-content')
+    const cardTitle = document.querySelector('.card-title')
+
+    errDiv.appendChild(document.createTextNode(error))
+    cardContent.insertBefore(errDiv, cardTitle)
+
+    setTimeout(clearErr, 3000)
+}
+
+function clearErr() {
+    document.getElementById('err').remove()
+}
